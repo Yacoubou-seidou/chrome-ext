aab6019441d173a3b6703eb1baba7d90236c19f4
Yacos <yacoseidou@gmail.com>
2022-04-01 15:57:58 +0100

    premier commit

---
+++ links.js
@@ -0,0 +1,10 @@
+/* const elem = document.getElementById("int").className;
+if (elem.className == "git") {
+  window.open("https://github.com/", "_blank").click();
+} else if (elem.className == "fb") {
+  window.open("https://www.facebook.com/", "_blank").click();
+} else if (elem.className == "uns") {
+  window.open("https://unsplash.com/", "_blank").click();
+} else {
+  window.open("https://intra.codeloccol.org/dashboard", "_blank").click();
+}  */
